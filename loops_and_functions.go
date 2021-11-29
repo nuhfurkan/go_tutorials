b09c80584a8900ca7395325667b5bfc998c98387
nuhfurkan <65976979+nuhfurkan@users.noreply.github.com>
2021-11-29 23:30:39 +0200

    Update loops_and_functions.go

--- loops_and_functions.go
+++ loops_and_functions.go
@@ -2,16 +2,21 @@ package main
 
 import (
 	"fmt"
-)
-
-var (
-
+	"math"
 )
 
 func Sqrt(x float64) float64 {
-	return 2
+	z := float64(x)
+	
+	// to set the number of significant figures change the variable sigFig
+	var sigFig float64 = 8
+	
+	for z*z - x > math.Pow(10, -sigFig) {
+		z -= (z*z - x) / (2*z)
+	}
+	return math.Round(z*math.Pow(10, sigFig))/ math.Pow(10, sigFig)
 }
 
 func main() {
-	fmt.Println(Sqrt(2))
+	fmt.Println(Sqrt(8))
 }
